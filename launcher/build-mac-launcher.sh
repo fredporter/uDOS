28365807227a18e08599529c9ec692725754bda1
“fredporter” <me@fredporter.com>
2025-07-05 23:41:24 +1000

    Update build-mac-launcher.sh

--- launcher/build-mac-launcher.sh
+++ launcher/build-mac-launcher.sh
@@ -64,6 +64,8 @@ cat > "$LAUNCH_SCRIPT" <<EOF
 APP_ROOT="\$(cd "\$(dirname "\$0")/../../.." && pwd)"
 chmod +x "\$APP_ROOT/scripts/start.sh"
 
+LAUNCH_CMD="cd \"\$APP_ROOT/launcher\"; bash uDOS_Run.sh"
+
 echo "APP_ROOT is: \$APP_ROOT" >> "\$APP_ROOT/launcher/uDOS_launcher_debug.log"
 echo "Attempting AppleScript-based launch..." >> "\$APP_ROOT/launcher/uDOS_launcher_debug.log"
 
@@ -71,7 +73,7 @@ echo "Attempting AppleScript-based launch..." >> "\$APP_ROOT/launcher/uDOS_launc
 tell application "Terminal"
   if not (exists window 1) then reopen
   activate
-  do script "cd \$APP_ROOT/launcher; bash uDOS_Run.sh"
+  do script "$LAUNCH_CMD"
 end tell
 OSA
 
